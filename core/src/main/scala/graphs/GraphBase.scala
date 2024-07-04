8f21f23ebb85b1243c695936de76a11a8ad5de5f
Alexis Faure <113013323+AlexiFa@users.noreply.github.com>
2024-07-04 15:52:21 +0200

    struct test

---
+++ core/src/main/scala/graphs/GraphBase.scala
@@ -0,0 +1,9 @@
+package graphs
+
+trait GraphBase[V, E] {
+  def vertices: Set[V]
+  def edges: Set[E]
+  def addEdge(v1: V, v2: V): GraphBase[V, E]
+  def removeEdge(v1: V, v2: V): GraphBase[V, E]
+  def neighbors(v: V): Set[V]
+}
