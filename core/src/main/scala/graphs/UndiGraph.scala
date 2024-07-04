8e04794caa5b7f5abc20f749e4c9258ecaf96d01
Alexis Faure <113013323+AlexiFa@users.noreply.github.com>
2024-07-04 16:25:29 +0200

    fix undigraph reverted edge

--- core/src/main/scala/graphs/UndiGraph.scala
+++ core/src/main/scala/graphs/UndiGraph.scala
@@ -5,8 +5,9 @@ case class UndiGraph(adjList: Map[Vertex, Set[Edge]]) extends GraphBase[Vertex, 
   def edges: Set[Edge] = adjList.values.flatten.toSet
   def addEdge(v1: Vertex, v2: Vertex, weight: Option[Int]): UndiGraph = {
     val edge = Edge(v1, v2, None)
+    val edgeReverted = Edge(v2, v1, None)
     val newAdjList = adjList.updatedWith(v1)(_.map(_ + edge).orElse(Some(Set(edge))))
-      .updatedWith(v2)(_.map(_ + edge).orElse(Some(Set(edge))))
+      .updatedWith(v2)(_.map(_ + edgeReverted).orElse(Some(Set(edgeReverted))))
     UndiGraph(newAdjList)
   }
   def removeEdge(v1: Vertex, v2: Vertex): UndiGraph = {
