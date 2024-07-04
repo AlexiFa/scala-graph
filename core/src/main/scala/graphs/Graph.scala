cd43f02b2bbfc5535b06a4543692e7c15a89d923
NakartSaW <ggl.clvr@gmail.com>
2024-07-04 14:20:21 +0200

    Ajout d'une fonction pour duplicate

--- core/src/main/scala/graphs/Graph.scala
+++ core/src/main/scala/graphs/Graph.scala
@@ -3,21 +3,23 @@ package graphs
 import zio.json._
 
 case class Graph(graphType: GraphType, adjacencyList: Map[Vertex, Set[Edge]]) {
-  def addEdge(from: Vertex, to: Vertex, weight: Option[Int] = None): Graph = {
-    val newEdge = Edge(to, weight)
-    val updateEdges = adjacencyList.get(from) match {
+
+  private def createEdge(vertex: Vertex, newEdge: Edge) = {
+    adjacencyList.get(vertex) match {
       case Some(edges) => edges + newEdge
       case None => Set(newEdge)
     }
+  }
+  
+  def addEdge(from: Vertex, to: Vertex, weight: Option[Int] = None): Graph = {
+    val newEdge = Edge(to, weight)
+    val updateEdges = createEdge(from, newEdge)
     val updateList = adjacencyList + (from -> updateEdges)
 
     graphType match {
       case GraphType.Undirected =>
         val reverseEdge = Edge(from, weight)
-        val updateReverseEdges = adjacencyList.get(to) match {
-          case Some(edges) => edges + reverseEdge
-          case None => Set(reverseEdge)
-        }
+        val updateReverseEdges = createEdge(to, reverseEdge)
         Graph(graphType, updateList + (to -> updateReverseEdges))
       case GraphType.Directed =>
         Graph(graphType, updateList)
@@ -39,5 +41,5 @@ case class Graph(graphType: GraphType, adjacencyList: Map[Vertex, Set[Edge]]) {
 }
 
 object Graph {
-  
+
 }
