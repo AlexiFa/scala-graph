caf6eedf222befc96b436e772b7854a7cc8f0a50
Alexis Faure <113013323+AlexiFa@users.noreply.github.com>
2024-07-12 15:45:11 +0200

    create test graph

--- app/src/main/scala/Main.scala
+++ app/src/main/scala/Main.scala
@@ -6,12 +6,13 @@
 //    printLine("Welcome to your first ZIO app!")
 import zio._
 import graphs._
-import graphs.GraphVizExtensions._
+//import graphs.GraphVizExtensions._
 
 object Main extends ZIOAppDefault {
   def run: ZIO[Environment & ZIOAppArgs & Scope, Any, Any] = {
     var graph = UndiGraph(Map("A" -> Set.empty, "B" -> Set.empty, "C" -> Set.empty))
-
+    graph = graph.addEdge(("A", "B", None))
+    graph = graph.addEdge(("B", "C", None))
 //    val e1 = Edge(v1, v2, None)
 //    val e2 = Edge(v2, v3, None)
     // val graph = UndiGraph(Map(v1 -> Set(e1), v2 -> Set(e2), v3 -> Set(e2)))
