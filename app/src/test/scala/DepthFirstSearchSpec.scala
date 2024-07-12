96e7e7292c4adcd83669c5cd5ca2b13241299c69
Alexis Faure <113013323+AlexiFa@users.noreply.github.com>
2024-07-12 19:19:08 +0200

    weight graph dfs works as well and testing for all types

--- app/src/test/scala/DepthFirstSearchSpec.scala
+++ app/src/test/scala/DepthFirstSearchSpec.scala
@@ -37,6 +37,17 @@ object DepthFirstSearchSpec extends ZIOSpecDefault{
         graph2 = graph2.addEdge(3, 3)
         val res2 = GraphOperationsImpl.DepthFirstSearch(graph2, 1, Set.empty)
         assertTrue(res2 == Set(1, 2, 0, 3))
+      },
+      test("WeightGraph") {
+        var weightGraph: WeightGraph[Any] = WeightGraph(Map(0 -> Set.empty, 1 -> Set.empty, 2 -> Set.empty, 3 -> Set.empty, 4 -> Set.empty))
+        weightGraph = weightGraph.addEdge(0, 1, 1)
+        weightGraph = weightGraph.addEdge(0, 2, 2)
+        weightGraph = weightGraph.addEdge(0, 3, 3)
+        weightGraph = weightGraph.addEdge(2, 3, 4)
+        weightGraph = weightGraph.addEdge(2, 4, 5)
+        weightGraph = weightGraph.addEdge(3, 2, 6)
+        val res = GraphOperationsImpl.DepthFirstSearch(weightGraph, 0, Set.empty)
+        assertTrue(res == Set(0, 1, 2, 3, 4))
       }
     )
 }
