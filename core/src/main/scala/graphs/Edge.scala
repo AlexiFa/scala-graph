19009f437314a9ac8c27a6d6dd7779f3e9e5d337
NakartSaW <ggl.clvr@gmail.com>
2024-07-12 16:34:03 +0200

    De-serialization
    Encoding & decoding json file

--- core/src/main/scala/graphs/Edge.scala
+++ core/src/main/scala/graphs/Edge.scala
@@ -1,3 +1,8 @@
 package graphs
 
 case class Edge[V](from: V, to: V, weight: Option[Int])
+
+object Edge {
+  implicit val decoder: JsonDecoder[Edge] = DeriveJsonDecoder.gen[Edge]
+  implicit val encoder: JsonEncoder[Edge] = DeriveJsonEncoder.gen[Edge]
+}
--- core/src/main/scala/graphs/Vertex.scala
+++ core/src/main/scala/graphs/Vertex.scala
@@ -1,3 +1,10 @@
 package graphs
 
+import zio.json.{DeriveJsonDecoder, DeriveJsonEncoder, JsonDecoder, JsonEncoder}
+
 case class Vertex(name: String)
+
+object Vertex {
+  implicit val decoder: JsonDecoder[Vertex] = JsonDecoder.string.map(Vertex(_))
+  implicit val encoder: JsonEncoder[Vertex] = JsonEncoder.string.contramap(_.name)
+}
